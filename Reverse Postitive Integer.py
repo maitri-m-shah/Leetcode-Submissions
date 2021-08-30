3129ce8f8d55a700ad5be764c5301420b391e4fd
Maitri Shah <55364652+maitri-m-shah@users.noreply.github.com>
2021-08-30 16:56:36 -0400

    Update Reverse Postitive Integer.py

--- Reverse Postitive Integer.py
+++ Reverse Postitive Integer.py
@@ -1,24 +1,20 @@
-//only for positive int
-
 class Solution:
     
     def reverse(self, x: int) -> int:
         
-        converted_num = str(x)
-        num_digits = len(converted_num)
+        x_string = str(x)
+        length = len(x_string)
+        stack = [] 
+        while length > 0:
+            last_char = x_string[-1]
+            x_string = x_string[0:len(x_string) - 1]
+            stack.append(last_char)
+            length = length -1 
         
-        finalDigit = ""
-        
-        if ( x = txt.isnumeric()):
-            while num_digits > 0:
-                lastDigit = int ()
-                lastDigit = x % 10 
-                lastDigit1 = str(lastDigit)
-
-                finalDigit = finalDigit + lastDigit1 
-                x = x//10
-                num_digits = num_digits -1
-
-            return finalDigit
-        
-        
+        length1 = len(str(x))
+        returnString = ""
+        while length1 > 0:
+            returnString += (stack.pop())
+            length1 = length1 - 1
+            
+        return returnString
